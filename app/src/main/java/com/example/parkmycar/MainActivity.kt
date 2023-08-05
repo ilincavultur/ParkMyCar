d88015c895b298a2ec7ca3b9d25550cca60c9314
ilincav00 <ilincav00@unet.univie.ac.at>
2023-08-05 14:38:41 +0300

    Splash Screen

--- app/src/main/java/com/example/parkmycar/MainActivity.kt
+++ app/src/main/java/com/example/parkmycar/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.parkmycar.ui.theme.ParkMyCarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         setContent {
             ParkMyCarTheme {
                 // A surface container using the 'background' color from the theme
